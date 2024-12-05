90ef68164ac1108e9be355c2304142bbcd7ba918
MIdasCO <rusabi54@gmail.com>
2024-12-05 10:33:39 +0600

    .

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import { Button } from "./components/Button"
 function App() {
   return(
     <div>
-      <Button variant = "bordered">Halal Boy</Button> 
-      <Button>Haram Boy</Button> 
+      <Button variant = "bordered" size="sm">Kick me!</Button> 
+      <Button size="md">Click Me!</Button> 
+      <Button variant = "bordered" size="lg">Kick me!</Button> 
+
     </div>
   )
 }
--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,6 +1,10 @@
 import "./index.css"
+
+//size = "sm" | "md" | "lg"
+
 export function Button(props){
-    const {variant,children} = props;
-    className = `my-button my-button--${variant}`
+    const {size, variant="primary",children} = props;
+    const className = `my-button my-button--${variant} my-button--${size}`
     return <button className={className}>{children}</button>
 }
+export default Button;
